7723240d7a5850a06e038b4e17b4c420db598cde
Edmund von der Burg <evdb@mysociety.org>
2012-06-30 14:40:24 +0100

    Minimal server for just the homepage

---
+++ app.js
@@ -0,0 +1,27 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    http = require('http');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on http://localhost:" + app.get('port'));
+});
